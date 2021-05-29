cf5ca15b31b2ba1fc6d837c8bfbd9204651264a4
Jeshika Maharjan <jeshika99@gmail.com>
2021-05-29 10:18:28 +0545

    nested if

---
+++ Assignment/Assignment_2/Odd_or_even.cpp
@@ -0,0 +1,24 @@
+//Program to check whether an integer is zero or even or odd.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter your number:"<<endl;
+    cin>>n;
+    if (n==0)
+    {
+        cout<<"Zero"<<endl;
+    }
+    else
+    {
+        if (n%2==0)
+        {
+            cout<<"Even"<<endl;
+        }
+        else
+        {
+            cout<<"Odd"<<endl;
+        }
+    }
+}
