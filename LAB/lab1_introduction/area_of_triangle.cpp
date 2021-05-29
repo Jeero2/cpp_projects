cdadc9a9f666f5944099a4fb31b266f9cd96ed05
Jeshika Maharjan <jeshika99@gmail.com>
2021-05-29 10:28:54 +0545

    practical assignments

---
+++ LAB/lab1_introduction/area_of_triangle.cpp
@@ -0,0 +1,17 @@
+//Program to find area of triangle using three sides of triangle
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    float area;
+    float s;
+    cout<<"\nenter three sides of triangle";
+    cin>>a>>b>>c;
+    s=((a+b+c)/2);
+    cout<<"\ns= "<<s;
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    cout<<"\narea of triangle is:"<<area;
+}
