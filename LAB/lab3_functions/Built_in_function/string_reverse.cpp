300fdf640b1c5756a6aa07d21fb7929e66c9abd4
Jeshika Maharjan <jeshika99@gmail.com>
2021-06-02 14:54:46 +0545

    use of strrev()

---
+++ LAB/lab3_functions/Built_in_function/string_reverse.cpp
@@ -0,0 +1,11 @@
+//program to reverse a string 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char string[50];
+    cout<<"Input your string:"<<endl;
+    cin>>string;
+    cout<<"Reversed string is:"<<strrev(string)<<endl;
+}
