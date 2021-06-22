a4b197ec65edec91ecc05838da676d2cdd5f86a4
Jeshika Maharjan <jeshika99@gmail.com>
2021-06-22 17:42:02 +0545

    Types of constructors

--- Notes/Constructors/copy_constructor.cpp
+++ Notes/Constructors/copy_constructor.cpp
@@ -14,14 +14,14 @@ class Wall {
     // parameterized constructor
     Wall(double len, double hgt) {
         // initialize private variables
-        length = len;
+        length = len;//len ko value length ma khanako
         height = hgt;
     }
 
     // copy constructor with a Wall object as parameter
     Wall(Wall &obj) {
         // initialize private variables
-        length = obj.length;
+        length = obj.length;//object ko through bata ava class ko data access garxa
         height = obj.height;
     }
     double calculateArea() {
@@ -38,7 +38,7 @@ int main() {
     cout << "Area of Wall 1: " << wall1.calculateArea() << endl;
 
     // copy contents of wall1 to another object wall2
-    Wall wall2 = wall1;
+    Wall wall2 = wall1;//wall1 as object pass hunxa mathi-- 10.5 ra 8.6 wall2 ma copy hunxa
 
     // print area of wall2
     cout << "Area of Wall 2: " << wall2.calculateArea() << endl;
