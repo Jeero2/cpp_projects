e9f7cf9562cd3d7ad69098ccd4fde5be893a5cfb
Jeshika Maharjan <jeshika99@gmail.com>
2021-07-14 12:12:37 +0545

    QN3 using constructor to initialize

---
+++ LAB/lab6_inheritance/QN4.cpp
@@ -0,0 +1,147 @@
+//QN3 using constructor
+#include<iostream>
+#include<string.h>
+using namespace std;
+class Person
+{
+    protected:
+        int code;
+        string name;
+    public:
+        Person()
+        {
+            code=0;
+            name="###";
+        }
+       void getData_Person()
+       {
+           cout<<"Enter code:"<<endl;
+           cin>>this->code;
+           cout<<"Enter name:"<<endl;
+           cin>>this->name;
+           cin.ignore();//getline le enter lai ni input liney vayeko le tyo enter lai ignore gara vanera command deko
+           
+       }
+  
+};
+class Account: public  virtual Person 
+{
+    protected:
+        int pay;
+    public:
+        Account()
+        {
+            code=0;
+            name="###";
+            pay=0;
+            
+        }
+        void getData_Account()
+        {
+            cout<<"Enter pay:"<<endl;
+            cin>>this->pay;
+        }
+        void display_Account()
+        {
+            cout<<"Name:"<<this->name<<endl;
+            cout<<"Code:"<<this->code<<endl;
+            cout<<"Pay:"<<this->pay<<endl;
+            
+        }
+
+};
+class Admin : public virtual Person
+{
+    protected:
+        string experience;
+    public:
+        Admin()
+        {
+            code=0;
+            name="###";
+            experience="###";
+        }
+        void getData_Admin()
+        {
+            cout<<"enter experience"<<endl;
+            getline(cin,this->experience);//string input ko lai getline easy hunxa
+        }
+        void display_Admin()
+        {
+            cout<<"Name:"<<this->name<<endl;
+            cout<<"Code:"<<this->code<<endl;
+            cout<<"Experience:"<<this->experience<<endl;
+        }
+            
+
+};
+class Master:public Account, public Admin 
+{
+    public:
+        Master()
+        {
+            cout<<"Creation of information:"<<endl;
+        }
+
+
+};
+int main()
+{
+   Master m1;
+   int choice;
+   char c;
+   do
+   {
+
+        cout<<"Press code according to choice"<<endl;
+        cout<<"Account:"<<"\t1"<<endl;
+        cout<<"Admin:"<<"\t2"<<endl;
+        cin>>choice;
+        if (choice==1)
+        {
+            m1.getData_Person();
+            m1.getData_Account();
+        }
+        else if (choice==2)
+        {
+            m1.getData_Person();
+            m1.getData_Admin();
+        }
+        else
+                cout<<"Invalid input"<<endl;
+        cout<<"Do you want to update information(Y/N):"<<endl;
+        cin>>c;
+   }
+   while(c=='Y'||c=='y');
+   cout<<"Displaying entered information"<<endl;
+
+   do
+   {        
+        cout<<"Press code according to choice"<<endl;
+        cout<<"Account:"<<"\t 1"<<endl;
+        cout<<"Admin:"<<"\t 2"<<endl;
+        cin>>choice;
+        if (choice==1)
+        {
+            m1.display_Account();
+        }
+        else if (choice==2)
+        {
+           m1.display_Admin();
+        }
+        else
+                cout<<"Invalid input"<<endl;
+        cout<<"Do you want to continue(Y/N):"<<endl;
+        cin>>c;
+   }
+   while(c=='Y'||c=='y');
+   return 0;
+
+}
+
+
+
+
+
+
+
