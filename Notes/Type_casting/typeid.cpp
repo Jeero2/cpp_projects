e98976cb3f28f0a514a3ad234639837b62919e07
Jeshika Maharjan <jeshika99@gmail.com>
2021-07-23 14:10:41 +0545

    Use of type_id

---
+++ Notes/Type_casting/typeid.cpp
@@ -0,0 +1,53 @@
+//TYPEID SHOWS WHAT TYPE IT IS 
+#include<iostream>
+#include<typeinfo>
+using namespace std;
+class Animal
+{
+    public:
+        virtual void display()
+        {
+
+        }
+};
+
+class Cow:public Animal
+{
+
+};
+class Dog:public Animal
+{
+
+};
+int main()
+{
+    int mark,roll;
+    float avg;
+    char *str;
+    Animal *anm;
+    Cow cw;
+    Dog dg;
+
+    cout<<"Type of mark is:"<<typeid(mark).name()<<endl;
+    cout<<"Type of roll is:"<<typeid(roll).name()<<endl;
+    cout<<"Type of avg is:"<<typeid(avg).name()<<endl;
+    cout<<"Type of *str is:"<<typeid(*str).name()<<endl;
+    cout<<"Type of str is:"<<typeid(str).name()<<endl;
+    
+    cout<<"Type of anm is:"<<typeid(anm).name()<<endl;
+    cout<<"Type of cw is:"<<typeid(cw).name()<<endl;
+    cout<<"Type of dg is:"<<typeid(dg).name()<<endl;
+
+    cout<<"*****************************"<<endl;
+    anm=&cw;
+    cout<<"type of *anm when pointing to cw is:"<<typeid(*anm).name()<<endl;
+    
+    anm=&dg;
+    cout<<"type of *anm when pointing to dg is:"<<typeid(*anm).name()<<endl;
+}
+//i=integer
+//f=float
+//c=character
+//Pc=character pointer
+//P6Animal=Animal class ko pointer variable
+//3Cow=Cow type
