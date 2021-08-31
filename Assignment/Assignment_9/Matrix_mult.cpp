d7f3447cedcb7033935ddef43e57513158dbadc9
Jeshika Maharjan <jeshika99@gmail.com>
2021-08-31 17:50:04 +0545

    .

---
+++ Assignment/Assignment_9/Matrix_mult.cpp
@@ -0,0 +1,67 @@
+//WAP a program to overload * operator to multiply two  3x3 matrix.
+#include<iostream>
+using namespace std;
+
+class Matrix
+{
+    public:
+        double matrix[3][3],matrix_product[3][3],sum=0;
+        int i,j,k;
+    void input()
+    {
+        cout<<"Enter values of matrix:"<<endl;
+        for (i = 1; i <=3; i++)
+        {
+            for(j=1;j<=3;j++)
+            {
+                cout<<"matrix"<<"["<<i<<"]"<<"["<<j<<"]"<<"=";
+                cin>>matrix[i][j];
+            }
+        }
+    }  
+
+    Matrix operator *(Matrix temp)
+    {
+        Matrix matrix_obj;
+        for (i=1;i<=3;i++)
+        {
+            for(j=1;j<=3;j++)
+            {
+                for (k=1;k<=3;k++)
+                {
+                    sum=sum+matrix[i][k]*temp.matrix[k][j];
+
+                }
+                matrix_obj.matrix_product[i][j]=sum;
+                sum=0;
+                  
+            }
+        }
+        return matrix_obj;
+    }
+    void display()
+    {
+        for (i=1;i<=3;i++)
+        {
+            for(j=1;j<=3;j++)
+            {
+                cout<<matrix_product[i][j]<<"  ";
+            }
+            cout<<"\n";
+        }
+                
+    }  
+};
+                
+int main()
+{
+    Matrix matrix_1,matrix_2,matrix_3;
+    matrix_1.input();
+    matrix_2.input();
+    matrix_3=matrix_1*matrix_2;
+    cout<<"Product:"<<endl;
+    matrix_3.display();
+}
+        
+
+
